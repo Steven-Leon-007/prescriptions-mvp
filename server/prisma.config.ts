5f7122d98f0be1301930925b8f6efbba4f4095a2
steven-leon-007 <estivlinyor@gmail.com>
2025-12-15 17:48:58 -0500

    fix: Update seed script path based on production environment

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, env } from 'prisma/config';
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'ts-node ./prisma/seed.ts',
+    seed: isProd
+      ? 'node dist/prisma/seed.js'
+      : 'ts-node prisma/seed.ts',
   },
   datasource: {
     url: env('DATABASE_URL'),
